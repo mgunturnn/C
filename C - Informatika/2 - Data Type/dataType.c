9b03953a7499f7e057815d05bfbef64a475d15eb
mgunturnn <guntur.nugraha@upi.edu>
2024-04-24 10:22:28 +0700

    create a new file

---
+++ C - Informatika/2 - Data Type/dataType.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int integerVar = 100;
+    double doubleVar = 20.0;
+    char charVar = 'G';
+
+    printf("Value of integerVar: %d\n", integerVar);
+    printf("Value of doubleVar: %lf\n", doubleVar);
+    printf("Value of charVar: %c\n", charVar);
+
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of char: %lu bytes\n", sizeof(char));
+
+    return 0;
+}
